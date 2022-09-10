580aa73cf1e63b9ba5d3fe943f3128dcae78ab64
Oleksii Kovenko <kovolexiy@gmail.com>
2022-09-10 16:47:25 +0300

    Calculate binary operations.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,49 @@
+﻿class Calculator
+{
+    static Stack<double> Numbers;
+    static Stack<Operation> Operations;
+
+    public static double Calculate(List<Token> tokens)
+    {
+        Numbers = new();
+        Operations = new();
+
+        foreach (var token in tokens)
+        {
+            switch (token)
+            {
+                case Number number:
+                    Numbers.Push(number.Value);
+                    break;
+
+                case Operation operation:
+                    while (Operations.TryPeek(out var top) && top.IsPriorThan(operation))
+                    {
+                        Calculate(top);
+                        Operations.Pop();
+                    }
+
+                    Operations.Push(operation);
+                    break;
+            }
+        }
+
+        while (Operations.TryPop(out var operation))
+        {
+            Calculate(operation);
+        }
+
+        return Numbers.Pop();
+    }
+
+    static void Calculate(Operation operation)
+    {
+        if (operation is BinaryOperation binary)
+        {
+            var right = Numbers.Pop();
+            var left = Numbers.Pop();
+            var result = binary.Calculate(left, right);
+            Numbers.Push(result);
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,2 @@
 ﻿var tokens = Tokenizer.Tokenize("4.2 + 9 / 42");
+var result = Calculator.Calculate(tokens);
